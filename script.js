da3b0486a8fc8fe02bd371de2a1d5e272b88a861
Toshiro <toshiro3457@gmail.com>
2022-05-30 17:07:30 -0400

    first div box created

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const container = document.getElementById('container')
+
+ const div = document.createElement('div')
+ div.classList.add('box')
+ container.appendChild(div)
+
+ const div1 = document.createElement('div')
+ div1.classList.add('box')
+ container.appendChild(div1)
+ 
 
