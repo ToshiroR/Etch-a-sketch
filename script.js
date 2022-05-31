66139d46e7c1930060e9342a792b8fcd6705fa8c
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2022-05-31 17:03:52 -0400

    still trying to make a 16x16 grid

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+//   container for the div elements
+
 const container = document.getElementById('container')
 
  
 
  
- 
+ //    creates multible div elements!
 
   for (i = 0; i < 16; i++) {
     let div = document.createElement('div');
