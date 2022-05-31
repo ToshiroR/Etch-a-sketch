687926164358ea22e1a2a1ae9d07af904a8f5d39
Toshiro <toshiro3457@gmail.com>
2022-05-31 14:56:18 -0400

    first 16 boxes are done

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.getElementById('container')
 
- const div = document.createElement('div')
- div.classList.add('box')
- container.appendChild(div)
+ //const div = document.createElement('div')
+ //div.classList.add('box')
+ //container.appendChild(div)
 
  const div1 = document.createElement('div')
  div1.classList.add('box')
  container.appendChild(div1)
  
 
+  for (i = 0; i < 16; i++) {
+    let div = document.createElement('div');
+    div.classList = 'box';
+    container.appendChild(div)
+  }
