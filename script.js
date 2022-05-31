ca2ebf3345e82948b2fd624972952594b13195fa
Toshiro <toshiro3457@gmail.com>
2022-05-31 15:14:07 -0400

    need to turn divs into a grid

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const container = document.getElementById('container')
 
- //const div = document.createElement('div')
- //div.classList.add('box')
- //container.appendChild(div)
+ 
 
- const div1 = document.createElement('div')
- div1.classList.add('box')
- container.appendChild(div1)
+ 
  
 
   for (i = 0; i < 16; i++) {
