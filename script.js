868056f9c1bebe04cae13d61df37a0e533bac73c
ToshiroR <97269007+ToshiroR@users.noreply.github.com>
2022-06-01 21:32:43 -0400

    Grid completed
    next thing to do is create a button to create the grid

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ const container = document.getElementById('container')
  
 
  
- //    creates multible div elements!
+ //    creates multible div elements into a Grid!
 
-  for (i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.classList = 'box';
-    container.appendChild(div)
+ function grid(number) {
+  for (i = 0; i < number; i++) {
+    let row = document.createElement('div');
+    row.classList = 'row';
+    container.appendChild(row)
+
+    for (x = 0; x < number; x++) {
+      let column = document.createElement('div');
+      column.classList = 'column';
+      row.appendChild(column)
+    }
+  
   }
+
+};
+grid(16)
